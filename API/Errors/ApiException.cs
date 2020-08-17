332b6f1ed843995b72a88067a0b4f066a852f7d5
Avinash <avinashchawla96@gmail.com>
2020-08-17 21:14:13 +0530

    Consistent Error Response

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,12 @@
+namespace API.Errors
+{
+    public class ApiException : ApiResponse
+    {
+        private readonly string Details;
+
+        public ApiException(int statusCode, string message = null, string details = null) : base(statusCode, message)
+        {
+            Details = details;
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -17,6 +17,8 @@ using Infrastructure.Data;
 using Core.Interfaces;
 using AutoMapper;
 using API.Helpers;
+using API.Middleware;
+using API.Errors;
 
 namespace API
 {
@@ -37,15 +39,32 @@ namespace API
             services.AddControllers();
             services.AddDbContext<Data.StoreContext>(x => 
                     x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            services.Configure<ApiBehaviorOptions>(options => 
+            {
+                options.InvalidModelStateResponseFactory = actionContext => 
+                {
+                    var errors = actionContext.ModelState
+                        .Where(e => e.Value.Errors.Count > 0)
+                        .SelectMany(x => x.Value.Errors)
+                        .Select(x => x.ErrorMessage).ToArray();
+
+                    var errorResponse = new ApiValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
+            
+            app.UseStatusCodePagesWithReExecute("error/{0}");
 
             app.UseHttpsRedirection();
 
